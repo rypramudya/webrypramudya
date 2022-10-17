b93f661e60bb558e4c0cbe96f07a7e1ad4e5b3a1
Rifky Pramudya <rifkypramudya7@gmail.com>
2022-10-17 12:18:39 +0700

    Masukin File

---
+++ js/dashboard.js
@@ -0,0 +1,11 @@
+/* globals Chart:false, feather:false */
+
+(() => {
+  "use strict";
+
+  feather.replace({ "aria-hidden": "true" });
+
+  // Graphs
+  const ctx = document.getElementById("myChart");
+  // eslint-disable-next-line no-unused-vars
+})();
